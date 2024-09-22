9d2f2293578d985a7cbf8222174e677a10bfe03c
Anton <anton@slutsky.com>
2024-09-22 13:12:18 -0400

    llm events

--- camera_driver.py
+++ camera_driver.py
@@ -16,106 +16,107 @@ computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredenti
 
 if __name__ == "__main__":
 
-    sleep_tm = 0.5
-
-    cam = VideoCapture(0)
-
-    prev_gray = None
-   
-    motion_dir = None
-
-    last_event_ts = None
-
-    motion_timeout = 7
-
-    SUPPORTED_OBJECTS = ["mammal", "cat", "animal", "person"]
-
     while True:
         try:
-            event_ts = datetime.datetime.now()
+            sleep_tm = 0.5
 
-            result, image = cam.read()
+            cam = VideoCapture(0)
 
-            hist = cv2.calcHist([image], [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])
-            hist = cv2.normalize(hist, hist).flatten()
+            prev_gray = None
+        
+            motion_dir = None
 
-            ts_now = event_ts.strftime('%Y-%m-%d_%H-%M-%S')
+            last_event_ts = None
 
-            d = cv2.compareHist(hist, prev_gray, cv2.HISTCMP_CORREL) if prev_gray is not None else 0
+            motion_timeout = 7
 
-            if d < 0.997:
+            SUPPORTED_OBJECTS = ["mammal", "cat", "animal", "person"]
 
-                is_success, buffer = cv2.imencode(".jpg", image)
-                io_buf = io.BytesIO(buffer)
-                
-                vision_response = computervision_client.detect_objects_in_stream(io_buf)
+            while True:
+                event_ts = datetime.datetime.now()
 
-                print(f"\nObjects analysis [{ts_now}]:")
-                
-                supported_found = False
-                
-                found_objects = []
+                result, image = cam.read()
 
-                for object in vision_response.objects:
-                    print(object.object_property, object.confidence)
-                    found_objects.append({
-                        "object_property" : object.object_property,
-                        "confidence" : object.confidence
-                    })
+                hist = cv2.calcHist([image], [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])
+                hist = cv2.normalize(hist, hist).flatten()
+
+                ts_now = event_ts.strftime('%Y-%m-%d_%H-%M-%S')
+
+                d = cv2.compareHist(hist, prev_gray, cv2.HISTCMP_CORREL) if prev_gray is not None else 0
+
+                if d < 0.997:
+
+                    is_success, buffer = cv2.imencode(".jpg", image)
+                    io_buf = io.BytesIO(buffer)
                     
-                    if object.object_property in SUPPORTED_OBJECTS and not supported_found and object.confidence > 0.8:
-                        supported_found = True
-                        print(f"Found supported object: {object.object_property}")
+                    vision_response = computervision_client.detect_objects_in_stream(io_buf)
 
-                info_obj = {
-                        "event_ts" : ts_now,
-                        "distance" : d,
-                        "objects" : found_objects
-                    }
-
-                with open("motions/latest_info.json", "w") as latest_info_json:
-                    latest_info_json.write(json.dumps(info_obj))
-
-                if supported_found:
+                    print(f"\nObjects analysis [{ts_now}]:")
                     
-                    event_duration = (event_ts - last_event_ts).total_seconds() if last_event_ts else 0
+                    supported_found = False
+                    
+                    found_objects = []
 
-                    if motion_dir is None or event_duration > motion_timeout:
-
-                        motion_dir = f"motions/motion_{ts_now}"
-                        info_dir = f"infos/info_{ts_now}"
+                    for object in vision_response.objects:
+                        print(object.object_property, object.confidence)
+                        found_objects.append({
+                            "object_property" : object.object_property,
+                            "confidence" : object.confidence
+                        })
                         
-                        print(f"Starting new event[{d}]: {motion_dir}. Time since last motion: {event_duration}")
+                        if object.object_property in SUPPORTED_OBJECTS and not supported_found and object.confidence > 0.8:
+                            supported_found = True
+                            print(f"Found supported object: {object.object_property}")
 
-                        os.makedirs(motion_dir)
-                        os.makedirs(info_dir)
+                    info_obj = {
+                            "event_ts" : ts_now,
+                            "distance" : d,
+                            "objects" : found_objects
+                        }
 
-                    last_event_ts = event_ts
+                    with open("motions/latest_info.json", "w") as latest_info_json:
+                        latest_info_json.write(json.dumps(info_obj))
 
-                    motion_path = f"{motion_dir}/event_{ts_now}.png"
-                    imwrite(motion_path, image)
+                    if supported_found:
+                        
+                        event_duration = (event_ts - last_event_ts).total_seconds() if last_event_ts else 0
 
-                    info_path = f"{info_dir}/info_{ts_now}.json"
-                    with open(info_path, "w") as info_json:
-                        info_json.write(json.dumps(info_obj))
+                        if motion_dir is None or event_duration > motion_timeout:
+
+                            motion_dir = f"motions/motion_{ts_now}"
+                            info_dir = f"infos/info_{ts_now}"
+                            
+                            print(f"Starting new event[{d}]: {motion_dir}. Time since last motion: {event_duration}")
+
+                            os.makedirs(motion_dir)
+                            os.makedirs(info_dir)
+
+                        last_event_ts = event_ts
+
+                        motion_path = f"{motion_dir}/event_{ts_now}.png"
+                        imwrite(motion_path, image)
+
+                        info_path = f"{info_dir}/info_{ts_now}.json"
+                        with open(info_path, "w") as info_json:
+                            info_json.write(json.dumps(info_obj))
 
 
-                    imwrite(f"motions/event_current.png", image)
+                        imwrite(f"motions/event_current.png", image)
 
-                    print(f"Motion recorded to: {motion_path}")
-                else:
-                    print(f"Ignoring [{ts_now}]")
+                        print(f"Motion recorded to: {motion_path}")
+                    else:
+                        print(f"Ignoring [{ts_now}]")
 
+                    
                 
-            
-            imwrite(f"motions/current_frame.png", image)
-            prev_gray = hist
+                imwrite(f"motions/current_frame.png", image)
+                prev_gray = hist
 
-    #        print(f"Sleeping for {sleep_tm} seconds. Last distance: {d}")
-            time.sleep(sleep_tm)
+        #        print(f"Sleeping for {sleep_tm} seconds. Last distance: {d}")
+                time.sleep(sleep_tm)
+                
+
+
+            cam.release()
         except:
-            print("ERROR: Camera read failed", sys.exc_info()[0])
-
-
-
-    cam.release()
+                print("ERROR: Camera read failed", sys.exc_info()[0])
