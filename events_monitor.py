9c52c12a4b027ed97bb2386c856bcbb310df2bfe
Anton <anton@slutsky.com>
2024-09-22 13:22:12 -0400

    llm events

--- events_monitor.py
+++ events_monitor.py
@@ -11,47 +11,56 @@ import run_smart_gate as rsg
 
 def process_event(self, src_path):
     self.lock.acquire()
-    if src_path not in self.event_threads:
-    
-        try:
-            os.system("aplay -D sysdefault:CARD=Headphones event_detected.wav")
-        except:
-            print("WARNING: Unable to play sound")
-                
-        self.event_threads.append(src_path)
 
-        print(f"Processing event: {src_path} events: {self.event_threads}")
-        time.sleep(5)
-        print("Checking dir:", src_path)
+    try:
         
-        data_actions = []
+        if src_path not in self.event_threads:
+        
+            try:
+                os.system("aplay -D sysdefault:CARD=Headphones event_detected.wav")
+            except:
+                print("WARNING: Unable to play sound")
 
-        events_root_dir = src_path
+            self.event_threads.append(src_path)
+
+            print(f"Processing event: {src_path} events: {self.event_threads}")
+            time.sleep(5)
+            print("Checking dir:", src_path)
+            
+            data_actions = []
+
+            events_root_dir = src_path
 
 
-        for filename in os.listdir(events_root_dir):
-            file_path = os.path.join(events_root_dir, filename)
-            if os.path.isfile(file_path):
-                data_actions.append({"type": "image", "path": filename})
+            for filename in os.listdir(events_root_dir):
+                file_path = os.path.join(events_root_dir, filename)
+                if os.path.isfile(file_path):
+                    data_actions.append({"type": "image", "path": filename})
 
-        print("Data Actions: ", data_actions)
+            print("Data Actions: ", data_actions)
 
-        prompt, image_urls = rsg.render_prompt(data_actions, images_root_dir = events_root_dir)
+            prompt, image_urls = rsg.render_prompt(data_actions, images_root_dir = events_root_dir)
 
-        print(f"---- Prompt:\n{prompt}\n----")
+            print(f"---- Prompt:\n{prompt}\n----")
 
-        lm_response = rsg.llm_task(user_prompt = prompt, 
-                system_prompt="What do you see in the pictures?", 
-                image_urls = image_urls)
+            lm_response = rsg.llm_task(user_prompt = prompt, 
+                    system_prompt="What do you see in the pictures?", 
+                    image_urls = image_urls)
 
-        print(f"==== Response:\n{lm_response}\n====")
+            print(f"==== Response:\n{lm_response}\n====")
 
-        self.event_threads.remove(src_path)
-        print(f"Releasing thread: {src_path}")
+            self.event_threads.remove(src_path)
+            print(f"Releasing thread: {src_path}")
 
-    else:
-        print("Race condition")
-    self.lock.release()
+        else:
+            print("Race condition")
+        
+    except:
+        print("ERROR: Event processing failed", sys.exc_info()[0])
+    finally:
+        self.lock.release()
+
+
 
 class EventHandler(FileSystemEventHandler):
     def __init__(self):
